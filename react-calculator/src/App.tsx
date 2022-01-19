31d0df06c2d6de322fef64fc9683f0aa19fea1de
nutthanon <nutthanon.tho@gmail.com>
2022-01-20 00:36:18 +0700

    add components

--- react-calculator/src/App.tsx
+++ react-calculator/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    background:
+      "linear-gradient(to bottom, #cffffe, #d3fcee, #dcf7e0, #e7f1d5, #f2ebcf, #f8e5cb, #fcdec9, #ffd8ca, #ffd0cc, #ffc9d4, #ffc4e1, #ffc1f3);",
+    minHeight: "100vh",
+  },
+});
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className={classes.root}>
+      <CssBaseline />
+      <Box />
+    </Box>
   );
-}
+};
 
 export default App;
---
+++ react-calculator/src/components/body.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+import { observer } from "mobx-react";
+
+const Body: React.FC = () => {
+  return (
+    <Box>
+      <p></p>
+    </Box>
+  );
+};
+
+export default Body;
