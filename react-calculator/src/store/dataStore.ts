47bba1ca87accae7f5f3ca1c5e900bde4a1dcd0e
nutthanon <nutthanon.tho@gmail.com>
2022-01-21 13:31:24 +0700

    update store

--- react-calculator/src/store/dataStore.ts
+++ react-calculator/src/store/dataStore.ts
@@ -2,6 +2,8 @@ import { makeObservable, action, observable } from "mobx";
 
 export class DataStoreImpl {
   @observable Data: string = "";
+  @observable Symbol: string = "";
+  @observable Cal: string = "";
 
   constructor() {
     makeObservable(this);
@@ -9,7 +11,29 @@ export class DataStoreImpl {
 
   @action
   change_data(word: string) {
-    this.Data += word;
+    const symbol: string[] = ["+", "-", "*", "÷"];
+    if (word === "C") {
+      this.Cal = "";
+      this.Data = "";
+    } else if (word === "=") {
+      if (this.Symbol == "+") {
+        this.Data = (parseFloat(this.Data) + parseFloat(this.Cal)).toString();
+      } else if (this.Symbol == "-") {
+        this.Data = (parseFloat(this.Data) - parseFloat(this.Cal)).toString();
+      } else if (this.Symbol == "*") {
+        this.Data = (parseFloat(this.Data) * parseFloat(this.Cal)).toString();
+      } else {
+        this.Data = (parseFloat(this.Cal) / parseFloat(this.Data)).toString();
+      }
+    } else {
+      if (symbol.includes(word)) {
+        this.Symbol = word;
+        this.Cal = this.Data;
+        this.Data = "";
+      } else {
+        this.Data += word;
+      }
+    }
   }
 }
 
