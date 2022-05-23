abaf690ebe153d96f092ec1620e9609baa272704
nutthanon <nutthanon.tho@gmail.com>
2022-05-23 23:49:12 +0700

    update Navbar.tsx

--- next-random-lottery-number/Components/Navbar.tsx
+++ next-random-lottery-number/Components/Navbar.tsx
@@ -6,10 +6,10 @@ import type { NextComponentType } from "next";
 
 const Navbar: NextComponentType = () => {
   return (
-    <nav className="p-1 flex flex-row items-center bg-white">
+    <nav className="p-1 flex flex-row items-center bg-white opacity-50">
       <p className="pl-5">เว็ปสุ่มเลข lottery</p>
       <div className="flex grow"></div>
-      <Tooltip title="Follow my Github" className="hover:bg-slate-200">
+      <Tooltip title="Follow my Githu b" className="hover:bg-slate-200">
         <IconButton>
           <FiGithub />
         </IconButton>
