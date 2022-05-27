b2557f2a829bf8fe1f96940fae743843c8c2ed7e
nutthanon <nutthanon.tho@gmail.com>
2022-05-28 02:01:09 +0700

    update index.tsx

--- next-random-lottery-number/pages/index.tsx
+++ next-random-lottery-number/pages/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
 import Navbar from "../Components/Navbar";
-import SixNumber from "../Components/sixNumber";
-import ThreeNumber from "../Components/threeNumber";
-import TwoNumber from "../Components/twoNumber";
 import Button from "@mui/material/Button";
+import BoxNumber from "../Components/BoxNumber";
 
 const Home: NextPage = () => {
   const [sixDigit, setSixDigit] = useState<string>("0");
@@ -27,16 +25,7 @@ const Home: NextPage = () => {
 
   return (
     <div className="min-h-screen bg-[#F4BCBC]">
-      <Navbar />
-      <div className="flex items-center border-2 min-h-screen justify-center flex-col space-y-4">
-        <SixNumber random_number={sixDigit} />
-        <div className="flex flex-row space-x-6">
-          <ThreeNumber />
-          <ThreeNumber />
-        </div>
-        <TwoNumber />
-        <Button onClick={handleClick}>Generate</Button>
-      </div>
+      <BoxNumber />
     </div>
   );
 };
