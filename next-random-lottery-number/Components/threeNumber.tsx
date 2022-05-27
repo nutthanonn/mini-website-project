29119ec03cb70dcdcaf11dc0e2336a430b76c554
nutthanon <nutthanon.tho@gmail.com>
2022-05-28 02:00:27 +0700

    delete components

--- next-random-lottery-number/Components/threeNumber.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { NextComponentType } from "next";
-
-const ThreeNumber:NextComponentType = () => {
-  return <div>ThreeNumber</div>;
-};
-
-export default ThreeNumber;
--- next-random-lottery-number/Components/twoNumber.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { NextComponentType } from "next";
-
-const TwoNumber: NextComponentType = () => {
-  return <div>TwoNumber</div>;
-};
-
-export default TwoNumber;
