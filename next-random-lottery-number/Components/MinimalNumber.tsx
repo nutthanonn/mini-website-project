49e48fb3879ab4ba3040bd3b3fc62d5dd3b64258
nutthanon <nutthanon.tho@gmail.com>
2022-05-28 17:36:40 +0700

    update element-id

--- next-random-lottery-number/Components/MinimalNumber.tsx
+++ next-random-lottery-number/Components/MinimalNumber.tsx
@@ -12,35 +12,35 @@ const MinimalNumber: React.FC<MinimalNumberProps> = (props) => {
       <div className="flex space-x-1" id="first-box">
         <div
           className="mt-2 h-[60.42px] w-[6.04px] rounded-lg bg-white"
-          id="first-y"
+          id="on-first-y-line"
         ></div>
         <div
           className="h-[7.25px] w-[48.34px] rounded-lg bg-white"
-          id="first-x"
+          id="first-x-line"
         ></div>
         <div
           className="mt-2 h-[60.42px] w-[6.04px] rounded-lg bg-white"
-          id="second-y"
+          id="on-last-y-line"
         ></div>
       </div>
       <div className="mx-2">
         <div
           className="h-[7.25px] w-[48.34px] rounded-lg bg-white"
-          id="first-x"
+          id="mid-x-line"
         ></div>
       </div>
       <div className="flex items-end" id="sec-box">
         <div
           className="mb-2 h-[60.42px] w-[6.04px] rounded-lg bg-white"
-          id="second-y"
+          id="under-first-y-line"
         ></div>
         <div
           className="mx-1 h-[7.25px] w-[48.34px] rounded-lg bg-white"
-          id="first-x"
+          id="last-x-line"
         ></div>
         <div
           className="mb-2 h-[60.42px] w-[6.04px] rounded-lg bg-white"
-          id="second-y"
+          id="under-last-y-line"
         ></div>
       </div>
     </div>
