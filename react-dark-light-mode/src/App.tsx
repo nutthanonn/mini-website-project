593ebe1beb652b225345206b0fca9f7830ac1f4a
nutthanon <nutthanon.tho@gmail.com>
2022-06-09 00:58:07 +0700

    :shrimp: success

--- react-dark-light-mode/src/App.tsx
+++ react-dark-light-mode/src/App.tsx
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
 import Switch from "./components/switch";
+import Lottie from "react-lottie";
+import animationSun from "./assets/sunclouds.json";
+import animationMoon from "./assets/clear-night-moon.json";
+
+const sunOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationSun,
+};
+
+const moonOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationMoon,
+};
 
 const App: React.FC = () => {
   const [isCheck, setIsCheck] = useState<boolean>(false);
@@ -10,11 +25,23 @@ const App: React.FC = () => {
 
   return (
     <div
-      className={`min-h-screen flex justify-center items-center ${
+      className={`min-h-screen flex justify-center items-center flex-col ${
         isCheck ? "bg-amber-200" : "bg-slate-700"
       }`}
     >
+      <div id="txt">
+        <p className="font-mono">{isCheck ? "Day" : "Night"}</p>
+      </div>
       <Switch light={isCheck} changeMode={handleChange} />
+      {isCheck ? (
+        <div className="absolute top-0 right-10 animate-sunBounce">
+          <Lottie options={sunOptions} isClickToPauseDisabled={true} />
+        </div>
+      ) : (
+        <div className="absolute top-0 right-0 animate-moonBounce">
+          <Lottie options={moonOptions} isClickToPauseDisabled={true} />
+        </div>
+      )}
     </div>
   );
 };
--- react-dark-light-mode/tailwind.config.js
+++ react-dark-light-mode/tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
         sun: "url(~/src/assets/sun.svg)",
         moon: "url(~/src/assets/moon.svg)",
       },
+      animation: {
+        moonBounce: "moonBounce 1s",
+        sunBounce: "sunBounce 1s",
+      },
+      keyframes: {
+        moonBounce: {
+          "0%": {
+            transform: "translateY(-100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateY(0%)",
+            opacity: 1,
+          },
+        },
+        sunBounce: {
+          "0%": {
+            transform: "translateY(20%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateY(0%)",
+            opacity: 1,
+          },
+        },
+      },
     },
   },
   plugins: [],
