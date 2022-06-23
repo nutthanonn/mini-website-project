638b4cbcc0fafd24f015f57bb860b5db11b11aaa
nutthanon <nutthanon.tho@gmail.com>
2022-06-23 16:59:35 +0700

    [+] update: change color

--- react-nutthanon-components/src/components/Chip.tsx
+++ react-nutthanon-components/src/components/Chip.tsx
@@ -3,28 +3,38 @@ import styled from "styled-components";
 
 interface ChipProps {
   title: string;
+  color?: string;
 }
 
 const ChipCustom = styled.div`
-  padding: 1px 10px;
-  border-radius: 20px;
-  box-shadow: 0 0 0 1.5px black;
+  width: fit-content;
+  height: fit-content;
+  padding: 1px 5px;
+  text-transform: capitalize;
+  border-radius: 10px;
+  box-shadow: 0 0 0 1px black;
   cursor: pointer;
   transition: 0.5s;
+  background-color: #706a6a;
+  font-size: 10px;
   &:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-    color: rgba(255, 255, 255, 0.7);
+    background-color: ${(props) => (props.color ? props.color : "black")};
     transition: 0.5s;
   }
-  color: white;
-  position: relative;
-  display: inline-block;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Chip: React.FC<ChipProps> = (props) => {
-  const { title } = props;
+  const { title, color } = props;
 
-  return <ChipCustom>{title}</ChipCustom>;
+  return (
+    <ChipCustom color={color}>
+      <p>{title}</p>
+    </ChipCustom>
+  );
 };
 
 export default Chip;
