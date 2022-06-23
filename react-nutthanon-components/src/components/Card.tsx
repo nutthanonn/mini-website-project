df503875b7e9bc2b41dbb989e615d920d201b7e8
nutthanon <nutthanon.tho@gmail.com>
2022-06-23 16:59:13 +0700

    [+] add: Card components

--- react-nutthanon-components/src/components/Card.tsx
+++ react-nutthanon-components/src/components/Card.tsx
@@ -1,21 +1,82 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../assets/fish.jpeg";
+import Chip from "./Chip";
 
 const CardCutom = styled.div`
   display: flex;
   flex-direction: column;
+  width: fit-content;
+  height: fit-content;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 20px;
+  background: linear-gradient(180deg, #333 60%, #000000 40%);
 `;
 
-const CardImg = styled.img``;
+const CardImg = styled.img`
+  border-radius: 50%;
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 0 0 2px rgba(90, 124, 247, 0.3);
+`;
 
-const CardTitle = styled.p``;
+const CardHeader = styled.div`
+  display: flex;
+  height: 50px;
+  width: 100%;
+  gap: 10px;
+`;
 
-const ScrdSubTitle = styled.p``;
+const CardTitle = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-const CardChip = styled.p``;
+const CardSubTitle = styled.p`
+  text-transform: capitalize;
+  color: white;
+`;
+
+const CardDetails = styled.p`
+  color: whitesmoke;
+  font-size: 30%;
+`;
+
+const CardChip = styled.div`
+  width: 100%;
+  height: 40px;
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
 
 const Card: React.FC = () => {
-  return <CardCutom>Card</CardCutom>;
+  return (
+    <CardCutom>
+      <CardHeader>
+        <CardImg src={Logo} />
+        <CardTitle>
+          <CardSubTitle>Nutthanon</CardSubTitle>
+          <CardDetails>&bull; &nbsp;Work in line thailand</CardDetails>
+        </CardTitle>
+      </CardHeader>
+      <CardChip>
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+        <Chip title="nutthanon" color="#fffada" />
+      </CardChip>
+    </CardCutom>
+  );
 };
 
 export default Card;
