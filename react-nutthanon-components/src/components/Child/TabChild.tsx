2af748f638fa41ce75354a42c09cf60cc61127f4
nutthanon <nutthanon.tho@gmail.com>
2022-06-24 17:10:46 +0700

    [+] add: add Tab components

---
+++ react-nutthanon-components/src/components/Child/TabChild.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TabChild: React.FC = () => {
+  return <div>TabChild</div>;
+};
+
+export default TabChild;
---
+++ react-nutthanon-components/src/components/Tab.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Tab: React.FC = () => {
+  return <div>Tab</div>;
+};
+
+export default Tab;
